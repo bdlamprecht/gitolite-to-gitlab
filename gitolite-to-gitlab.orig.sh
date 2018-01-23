f12d72174a38dcc73ae1b7a196ca8ce362be994c
Brady Lamprecht <bl839s@att.com>
2018-01-22 19:16:30 -0700

    Added a '.gitignore' to ignore working directory for migrated repositories.

--- gitolite-to-gitlab.orig.sh
+++ gitolite-to-gitlab.orig.sh
@@ -17,14 +17,14 @@ ssh git@$GITOLITE info | awk '/^[ @]*R/{print $NF}' | while read REPO_NAME
 do
 	REPO_NAME2=$(echo $REPO_NAME | sed -e 's/\//_/g')
 	REPO_NAME3=$(echo $REPO_NAME | sed 's@.*/@@')
- 	echo $REPO_NAME
-	echo $REPO_NAME2
-	echo $REPO_NAME3
-  echo	git clone --mirror git@$GITOLITE:$REPO_NAME.git
-  echo	curl -v -H "Content-Type:application/json" $GITLAB_URL/api/v3/projects?private_token=$API_TOKEN -d "{ \"name\": \"$REPO_NAME2\" }"
-  echo	cd $REPO_NAME3.git
-  echo	git remote set-url origin git@$GITLAB_URL:$GITLAB_USER/$REPO_NAME2.git
-  echo  git push
-  echo 	cd ..
+#	echo $REPO_NAME
+#	echo $REPO_NAME2
+#	echo $REPO_NAME3
+	git clone --mirror git@$GITOLITE:$REPO_NAME.git
+	curl -v -H "Content-Type:application/json" $GITLAB_URL/api/v3/projects?private_token=$API_TOKEN -d "{ \"name\": \"$REPO_NAME2\" }"
+	cd $REPO_NAME3.git
+	git remote set-url origin git@$GITLAB_URL:$GITLAB_USER/$REPO_NAME2.git
+	git push
+	cd ..
 done
 
